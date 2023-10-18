fa200d1f4a0809499e16a4323964431aec156216
Caian J. Santana <lesssking@gmail.com>
2023-10-18 20:16:33 -0300

    Add files via upload

---
+++ medOfficeAPI/doctorAPI/src/main/java/com/br/doctorAPI/configuration/Configuration.java
@@ -0,0 +1,13 @@
+package com.br.doctorAPI.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public ModelMapper obterModelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ medOfficeAPI/patientAPI/src/main/java/com/br/patientAPI/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package com.br.patientAPI.configuration;
+
+public class Configuration {
+
+	public Configuration() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
