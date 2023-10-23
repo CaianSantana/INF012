877096dd786beccba5a48b7fb07a933e55437b6e
Caian J. Santana <lesssking@gmail.com>
2023-10-23 17:13:21 -0300

    Delete medOfficeAPI/medConsultAPI/src/main/java/com/br/medConsultAPI/repositories/DateRepository.java

--- medOfficeAPI/medConsultAPI/src/main/java/com/br/medConsultAPI/repositories/DateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.medConsultAPI.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.medConsultAPI.model.Date;
-
-public interface DateRepository extends JpaRepository<Long, Date>{
-
-}
